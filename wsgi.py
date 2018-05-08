8fdfe62ac43c4fa4b48270ed4f42dd186a8e0874
Blake Easley <thejimmyscene@gmail.com>
2018-05-07 21:54:30 -0400

    Adding flask dreams

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,5 @@
-"""
-WSGI config for project project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+from app import application
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-
-application = get_wsgi_application()
+if __name__ == "__main__":
+    application.run()
